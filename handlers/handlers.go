45079bb773820f264e25a1d3d2460b8055e15126
unfavorablenode <unfavorable_node@proton.me>
2022-07-05 14:49:38 +0000

    Added gracefull shutdown and moved handler code around a bit making it easier to find where which code will be later on

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,19 +1,55 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+	"log"
+	"os"
+	"context"
+	"os/signal"
 
 	"github.com/gorilla/mux"
 )
 
-func RegisterRoutes() *mux.Router {
+func GetRouterWithRoutes() *mux.Router {
     r := mux.NewRouter()
-
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Testing")
-    })
-
+    r.HandleFunc("/", indexHandlerGet).Methods("GET")
     return r;
+}
+
+func GetServer(Router *mux.Router) *http.Server	    {
+    return &http.Server{
+	Handler:	Router,
+	Addr:		"127.0.0.1:8000",
+	IdleTimeout:	time.Second * 60,
+	WriteTimeout:	15 * time.Second,
+	ReadTimeout:	15 * time.Second,
+    }
+}
+
+func StartServerWithGracefullShutdown(Server *http.Server)	{
+    var wait = time.Second * 15
+
+    go func()	{
+	if err := Server.ListenAndServe(); err != nil  {
+	    log.Println(err)
+	}
+    }()
+
+    c := make(chan os.Signal, 1)
+    // We'll accept graceful shutdowns when quit via SIGINT (Ctrl+c)
+    // SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+    signal.Notify(c, os.Interrupt)
+
+    // Block until we receive out signal.
+    <-c
+
+    // Create a deadline to wait for
+    ctx, cancel := context.WithTimeout(context.Background(), wait)
+    defer cancel()
+
+    Server.Shutdown(ctx)
+
+    log.Println("Shutting down")
+    os.Exit(0)
 }
--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/unfavorablenode/thin_node/handlers"
 )
 
+
 func main() {
-    r := handlers.RegisterRoutes()
-    srv := &http.Server{
-	Handler:	r,
-	Addr:		"127.0.0.1:8000",
-	WriteTimeout:	15 *time.Second,
-	ReadTimeout:	15 * time.Second,
-    }
-    log.Fatal(srv.ListenAndServe())
+    Router := handlers.GetRouterWithRoutes()
+    Server := handlers.GetServer(Router)
+    handlers.StartServerWithGracefullShutdown(Server)
 }
